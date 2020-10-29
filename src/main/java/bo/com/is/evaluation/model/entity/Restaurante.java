309bcbe2e52015eca83856a6b963b9c4a13eb1b3
Juan Jose Miranda <juanjose@Juans-MacBook-Pro.local>
2020-10-28 21:11:42 -0400

    add swagger2

--- src/main/java/bo/com/is/evaluation/model/entity/Restaurante.java
+++ src/main/java/bo/com/is/evaluation/model/entity/Restaurante.java
@@ -1,6 +1,10 @@
 package bo.com.is.evaluation.model.entity;
 
 import bo.com.is.evaluation.dto.TipoComidaDto;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
@@ -9,6 +13,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @Entity
+@Data @Builder @AllArgsConstructor @NoArgsConstructor
 @Table(name = "RES_RESTAURANTES")
 public class Restaurante {
     @Id
@@ -83,172 +88,4 @@ public class Restaurante {
     @ManyToOne
     @JoinColumn(name = "id_tipo_comida", insertable = false, updatable = false)
     private TipoComida tipoComida;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getDescripcion() {
-        return descripcion;
-    }
-
-    public void setDescripcion(String descripcion) {
-        this.descripcion = descripcion;
-    }
-
-    public Integer getTelefono() {
-        return telefono;
-    }
-
-    public void setTelefono(Integer telefono) {
-        this.telefono = telefono;
-    }
-
-    public String getEstado() {
-        return estado;
-    }
-
-    public void setEstado(String estado) {
-        this.estado = estado;
-    }
-
-    public String getLogo() {
-        return logo;
-    }
-
-    public void setLogo(String logo) {
-        this.logo = logo;
-    }
-
-    public String getHorario() {
-        return horario;
-    }
-
-    public void setHorario(String horario) {
-        this.horario = horario;
-    }
-
-    public String getLatitud() {
-        return latitud;
-    }
-
-    public void setLatitud(String latitud) {
-        this.latitud = latitud;
-    }
-
-    public String getLongitud() {
-        return longitud;
-    }
-
-    public void setLongitud(String longitud) {
-        this.longitud = longitud;
-    }
-
-    public String getDireccion() {
-        return direccion;
-    }
-
-    public void setDireccion(String direccion) {
-        this.direccion = direccion;
-    }
-
-    public Integer getIdTipoComida() {
-        return idTipoComida;
-    }
-
-    public void setIdTipoComida(Integer idTipoComida) {
-        this.idTipoComida = idTipoComida;
-    }
-
-    public LocalDateTime getFechaAlta() {
-        return fechaAlta;
-    }
-
-    public void setFechaAlta(LocalDateTime fechaAlta) {
-        this.fechaAlta = fechaAlta;
-    }
-
-    public Integer getIdUsuarioAlta() {
-        return idUsuarioAlta;
-    }
-
-    public void setIdUsuarioAlta(Integer idUsuarioAlta) {
-        this.idUsuarioAlta = idUsuarioAlta;
-    }
-
-    public LocalDateTime getFechaBaja() {
-        return fechaBaja;
-    }
-
-    public void setFechaBaja(LocalDateTime fechaBaja) {
-        this.fechaBaja = fechaBaja;
-    }
-
-    public Integer getIdUsuarioBaja() {
-        return idUsuarioBaja;
-    }
-
-    public void setIdUsuarioBaja(Integer idUsuarioBaja) {
-        this.idUsuarioBaja = idUsuarioBaja;
-    }
-
-    public LocalDateTime getFechaDesde() {
-        return fechaDesde;
-    }
-
-    public void setFechaDesde(LocalDateTime fechaDesde) {
-        this.fechaDesde = fechaDesde;
-    }
-
-    public Integer getIdUsuarioDesde() {
-        return idUsuarioDesde;
-    }
-
-    public void setIdUsuarioDesde(Integer idUsuarioDesde) {
-        this.idUsuarioDesde = idUsuarioDesde;
-    }
-
-    public LocalDateTime getFechaHasta() {
-        return fechaHasta;
-    }
-
-    public void setFechaHasta(LocalDateTime fechaHasta) {
-        this.fechaHasta = fechaHasta;
-    }
-
-    public Integer getIdUsuarioHasta() {
-        return idUsuarioHasta;
-    }
-
-    public void setIdUsuarioHasta(Integer idUsuarioHasta) {
-        this.idUsuarioHasta = idUsuarioHasta;
-    }
-
-    public TipoComida getTipoComida() {
-        return tipoComida;
-    }
-
-    public void setTipoComida(TipoComida tipoComida) {
-        this.tipoComida = tipoComida;
-    }
-
-    public List<RestauranteAmbiente> getRestauranteAmbientes() {
-        return restauranteAmbientes;
-    }
-
-    public void setRestauranteAmbientes(List<RestauranteAmbiente> restauranteAmbientes) {
-        this.restauranteAmbientes = restauranteAmbientes;
-    }
 }
